eb8014cadeef96edcb2324db718282c82afebea3
Rajesh Bathula <greeshma-sadineni.com>
2020-05-14 20:42:27 +0200

    first commit

---
+++ Switch Switch/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Switch_Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter number:");
+            num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 0:
+                    
+                case 1:
+                  case 2:
+                   
+                case 3:
+                    
+                case 4:
+                    Console.WriteLine("fail");
+                    break;
+                case 5:
+                    Console.WriteLine("pass");
+                    break;
+                case 6:
+                    Console.WriteLine("Good");
+                    break;
+                case 7:
+                   
+                case 8:
+                    Console.WriteLine("Notable");
+                    break;
+                case 9:
+                   
+                case 10:
+                    Console.WriteLine("Excellent");
+                    break;
+                default :
+                    Console.WriteLine("No not exits");
+                    break;
+                    Console.Read();
+            }
+
+            
+        }
+
+    }
+}
